20d390da489ce9a92d7d82e9fbeba79f34199e77
Martin Jacobsen <controloptioncommand@gmail.com>
2019-03-05 19:54:57 +0100

    Removed redundant files

--- packages/gatsby/src/pages/index.jsx
+++ packages/gatsby/src/pages/index.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { Link } from 'gatsby';
 import PageWrapper from '../components/PageWrapper';
 
 export default () => (
-  <PageWrapper pageType="index" pageData={{ page: { title: 'Ducks & Dogs' } }}>
-    Lorem ipsum dolor sit amet.
-    <Link to="/grf">Go!</Link>
+  <PageWrapper>
+    Booyah!
   </PageWrapper>
 );
--- packages/gatsby/src/styles/media.js
+++
@@ -1,1 +0,0 @@
-export const mediaMinSmall = `(min-width: 450px)`;
